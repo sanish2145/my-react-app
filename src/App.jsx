8039bf0e09910c90238542d5ee990f3587c2cb62
sanish2145 <vikramkishor266@gmail.com>
2025-05-18 22:29:58 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import Home from '../Pages/Home'
 import Projects from '../Pages/Projects'
 import Skills from '../Pages/Skills'
 import Blog from '../Pages/Blog'
-import Contact from ./Pages/Contact'
+import Contact from './Pages/Contact'
+
 
 export default function App() {
   const [activeSection, setActiveSection] = useState('home')
