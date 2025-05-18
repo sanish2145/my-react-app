72c8ed831ff5c46be596e8230a8552686fd315e5
sanish2145 <vikramkishor266@gmail.com>
2025-05-18 22:22:52 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import Navbar from '../components/Navbar'
+import Navbar from './Navbar'
 import Home from './Home'
 import Projects from './Projects'
 import Skills from './Skills'
